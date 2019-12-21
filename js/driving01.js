3dff5cfe3ddd9d8840a329203789b2567833fcd3
cd72 <chris.j.denney@gmail.com>
2019-12-21 02:06:01 +0000

    scale manager

--- js/driving01.js
+++ js/driving01.js
@@ -2,6 +2,10 @@ var config = {
     type: Phaser.AUTO,
     width: 360,
     height: 640,
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH
+    },
     physics: {
         default: 'matter',
         matter: {
